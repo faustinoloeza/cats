2db8e262dd08183d05ae1ff52b3cb9de6cd20e76
Faustino Loeza Pérez <faustinoloezaperez@gmail.com>
2020-09-15 03:43:56 -0500

    Updates

---
+++ precache-manifest.1767711274bda1c14b4e65196e3c9ca6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b068a6f2b41758704fa1e68bbe12baa",
+    "url": "/cats/index.html"
+  },
+  {
+    "revision": "388f084d1822e85a7898",
+    "url": "/cats/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b54197703cfe946b2437",
+    "url": "/cats/static/js/2.8f108d5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cats/static/js/2.8f108d5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388f084d1822e85a7898",
+    "url": "/cats/static/js/main.ba9cd5ca.chunk.js"
+  },
+  {
+    "revision": "2132597c5ed3818d1ee7",
+    "url": "/cats/static/js/runtime-main.94ec4682.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cats/static/media/logo.5d5d9eef.svg"
+  }
+]);
